fe3d57259758b1f1c2f2e34e0de146f6004e9236
Moh. Iqbal Rafsanjani <iqbal.outlaw@gmail.com>
2022-06-11 20:07:54 +0700

    Add GET (id), UPDATE, DELETE

--- FavoriteMovies/FavoriteMovies/Controllers/FavoriteMovieController.cs
+++ FavoriteMovies/FavoriteMovies/Controllers/FavoriteMovieController.cs
@@ -20,7 +20,15 @@ namespace FavoriteMovies.Controllers
                     Genre = "Action/Adventure",
                     Director = "George Miller",
                     ReleaseDate = DateTime.Parse("2015/05/14"),
-                }
+                },
+                new FavoriteMovie
+                {
+                    Id = 2,
+                    Title = "The Hateful Eight",
+                    Genre = "Western/Drama",
+                    Director = "Quentin Tarantino",
+                    ReleaseDate = DateTime.Parse("2016/01/19"),
+                },
             };
 
         [HttpGet]
@@ -28,11 +36,50 @@ namespace FavoriteMovies.Controllers
         {
             return Ok(movies);
         }
-
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FavoriteMovie>> GetMovie(int id)
+        {
+            var movie = movies.Find(m => m.Id == id);
+            if (movie == null)
+            {
+                return BadRequest("Movie not found!");
+            }
+            return Ok(movie);
+        }
+        
         [HttpPost]
         public async Task<ActionResult<List<FavoriteMovie>>> AddMovie(FavoriteMovie movie)
         {
             movies.Add(movie);
+            return Ok(movies);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<FavoriteMovie>>> UpdateMovie(FavoriteMovie request)
+        {
+            var movie = movies.Find(m => m.Id == request.Id);
+            if (movie == null)
+            {
+                return BadRequest("Movie not found!");
+            }
+            movie.Title = request.Title;
+            movie.Genre = request.Genre;
+            movie.Director = request.Director;
+            movie.ReleaseDate = request.ReleaseDate;
+            
+            return Ok(movies);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<FavoriteMovie>> DeleteMovie(int id)
+        {
+            var movie = movies.Find(m => m.Id == id);
+            if (movie == null)
+            {
+                return BadRequest("Movie not found!");
+            }
+            movies.Remove(movie);
             return Ok(movies);
         }
     }
